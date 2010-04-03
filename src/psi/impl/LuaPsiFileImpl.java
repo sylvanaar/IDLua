431549d288a754a79914756ba32b979b51a8037a
sylvanaar <sylvanaar@mindspring.com>
2010-04-03 14:52:19 -0400

    removed files

--- src/psi/impl/LuaPsiFileImpl.java
+++ src/psi/impl/LuaPsiFileImpl.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2009 Max Ishchenko
+ * Copyright 2010 Jon S Akhtar (Sylvanaar)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package com.sylvanaar.idea.Lua.psi.impl;
@@ -19,9 +19,7 @@ package com.sylvanaar.idea.Lua.psi.impl;
 import com.intellij.extapi.psi.PsiFileBase;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.psi.FileViewProvider;
-//import com.sylvanaar.idea.Lua.LuaFileTypeManager;
 import com.sylvanaar.idea.Lua.LuaFileType;
-import com.sylvanaar.idea.Lua.LuaLanguage;
 import com.sylvanaar.idea.Lua.psi.LuaPsiFile;
 import org.jetbrains.annotations.NotNull;
 
