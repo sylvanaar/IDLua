6c4d5a7387d5ab85bcaaf921471dcefb5a82aa58
sylvanaar <none@none>
2010-05-09 01:08:13 -0400

    support for the Kahlua interactive shell

---
+++ src/LuaInterpreterWindowFactory.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2010 Jon S Akhtar (Sylvanaar)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import se.krka.kahlua.converter.KahluaConverterManager;
+import se.krka.kahlua.converter.KahluaEnumConverter;
+import se.krka.kahlua.converter.KahluaNumberConverter;
+import se.krka.kahlua.converter.KahluaTableConverter;
+import se.krka.kahlua.integration.expose.LuaJavaClassExposer;
+import se.krka.kahlua.j2se.J2SEPlatform;
+import se.krka.kahlua.j2se.interpreter.InteractiveShell;
+import se.krka.kahlua.vm.KahluaTable;
+import se.krka.kahlua.vm.Platform;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jon S Akhtar
+ * Date: May 7, 2010
+ * Time: 8:02:20 PM
+ */
+public class LuaInterpreterWindowFactory implements ToolWindowFactory {
+    @Override
+    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+        final Platform platform = new J2SEPlatform();
+        final KahluaTable env = platform.newEnvironment();
+
+        KahluaConverterManager manager = new KahluaConverterManager();
+        KahluaNumberConverter.install(manager);
+        KahluaEnumConverter.install(manager);
+        new KahluaTableConverter(platform).install(manager);
+        LuaJavaClassExposer exposer = new LuaJavaClassExposer(manager, platform, env);
+
+        KahluaTable staticBase = platform.newTable();
+        env.rawset("Java", staticBase);
+
+        InteractiveShell shell = new InteractiveShell("Kahlua interactive shell", platform, env);
+        shell.appendWelcome();
+
+        toolWindow.getComponent().add(shell.getWindow(0));
+    }
+}
