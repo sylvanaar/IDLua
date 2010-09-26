1b32b1300d270e2114a86be1c6ca20890a10ec66
sylvanaar <none@none>
2010-09-26 06:16:58 -0400

    updated for 0.8.17

--- test/lua5.1-tests/simple.lua
+++ test/lua5.1-tests/simple.lua
@@ -7,53 +7,52 @@ local foo = foo
 
 -- I am testing globals/locals
 
-function a(b,c,d)
-  a,b = c,d
-  return self
+function a(b, c, d)
+    a, b = c, d
+    return self
 end
 
 local function a()
-  a,b = c,d
-  return self
+    a, b = c, d
+    return self
 end
 
 local t = {}
-function t:b(c,d)
-  return self
+function t:b(c, d)
+    return self
 end
 
 local foo = foo
 
 local a = function()
     return
-end
+    end
 
 b.c = function()
-    return 
-end
+    return
+    end
 
 a.b.c.d = function()
     return
+    end
+
+
+local a, b, c, d, e, f = 1, 2, 3, 4
+
+
+for k, v in pairs(t) do
 end
 
-
-local a,b,c,d,e,f = 1,2,3,4
-
-
-for k,v in pairs(t) do
-end
-
-for i=1,10 do
+for i = 1, 10 do
 end
 
 local x1 = function()
-        print( "local disappear: " .. tostring(x1) )            -- prints "nil"
-end
+    print("local disappear: " .. tostring(x1)) -- prints "nil"
+    end
 
 
 function a()
-
-
+    
     print(a)
 end
 
