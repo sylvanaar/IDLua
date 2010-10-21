5eff78df71caafb6c2b0ba1269e5a476848ae9f5
sylvanaar <none@none>
2010-10-21 07:22:36 -0400

    cleaned up the java help for the lua manual. gave it an icon and collapsed it by default

--- help/Lua.hs
+++ help/Lua.hs
@@ -1,9 +1,9 @@
 <?xml version='1.0' encoding='ISO-8859-1' ?>
 <!DOCTYPE helpset
   PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
-         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
-<helpset version="1.0">
+<helpset version="2.0">
 
   <!-- title -->
   <title>Lua Manual</title>
@@ -21,5 +21,6 @@
     <type>javax.help.TOCView</type>
     <data>LuaTOC.xml</data>
   </view>
+
 
 </helpset>
