a498944c191de820263464a72308f9386644514d
sylvanaar <none@none>
2010-11-27 19:20:53 -0500

    update some completion related files

--- src/findUsages/LuaWordsScanner.java
+++ src/findUsages/LuaWordsScanner.java
@@ -1,18 +1,19 @@
 /*
  * Copyright 2010 Jon S Akhtar (Sylvanaar)
- *  
+ *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
- *  
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *   Unless required by applicable law or agreed to in writing, software
  *   distributed under the License is distributed on an "AS IS" BASIS,
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
+
 package com.sylvanaar.idea.Lua.findUsages;
 
 import com.intellij.lang.cacheBuilder.WordOccurrence;
@@ -24,9 +25,6 @@ import com.sylvanaar.idea.Lua.lang.lexer.LuaLexer;
 import com.sylvanaar.idea.Lua.lang.lexer.LuaTokenTypes;
 
 
-/**
- * @author ven
- */
 class LuaWordsScanner implements WordsScanner {
     private final Lexer myLexer;
 
