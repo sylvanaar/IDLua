ee179561f6b3a479b33d25762c0fc667f906d45b
sylvanaar <none@none>
2010-12-17 05:58:52 -0500

    goto symbol

--- src/editor/LuaGotoSymbolContributor.java
+++ src/editor/LuaGotoSymbolContributor.java
@@ -17,8 +17,28 @@
 package com.sylvanaar.idea.Lua.editor;
 
 import com.intellij.navigation.ChooseByNameContributor;
+import com.intellij.navigation.ItemPresentation;
 import com.intellij.navigation.NavigationItem;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ContentIterator;
+import com.intellij.openapi.roots.FileIndex;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.impl.FakePsiElement;
+import com.sylvanaar.idea.Lua.LuaFileType;
+import com.sylvanaar.idea.Lua.lang.psi.LuaPsiFile;
+import com.sylvanaar.idea.Lua.lang.psi.statements.LuaFunctionDefinitionStatement;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,11 +50,141 @@ public class LuaGotoSymbolContributor implements ChooseByNameContributor {
     @Override
     public String[] getNames(Project project, boolean b) {
 
-        return new String[0];
+        FileIndex fi = ProjectRootManager.getInstance(project).getFileIndex();
+
+        final List<String> names = new ArrayList<String>();
+
+        final Project myProject = project;
+
+        fi.iterateContent(new ContentIterator() {
+            @Override
+            public boolean processFile(VirtualFile fileOrDir) {
+                if (fileOrDir.getFileType() == LuaFileType.LUA_FILE_TYPE) {
+                    PsiFile file = PsiManager.getInstance(myProject).findFile(fileOrDir);
+
+                    if (file instanceof LuaPsiFile) {
+                        final LuaPsiFile lua = (LuaPsiFile) file;
+
+                        for(LuaFunctionDefinitionStatement func: lua.getFunctionDefs()) {
+                            names.add(func.getName());
+                        }
+                    }
+
+                }
+                return true;
+            }
+        });
+        return names.toArray(new String[names.size()]);
     }
 
     @Override
     public NavigationItem[] getItemsByName(String s, String s1, Project project, boolean b) {
-        return new NavigationItem[0];
+        FileIndex fi = ProjectRootManager.getInstance(project).getFileIndex();
+
+        final List<NavigationItem> names = new ArrayList<NavigationItem>();
+
+        final Project myProject = project;
+        final String chosenName = s;
+
+        fi.iterateContent(new ContentIterator() {
+            @Override
+            public boolean processFile(VirtualFile fileOrDir) {
+                if (fileOrDir.getFileType() == LuaFileType.LUA_FILE_TYPE) {
+                    PsiFile file = PsiManager.getInstance(myProject).findFile(fileOrDir);
+
+                    if (file instanceof LuaPsiFile) {
+                        final LuaPsiFile lua = (LuaPsiFile) file;
+
+                        for(LuaFunctionDefinitionStatement func: lua.getFunctionDefs()) {
+                            if (func.getName().equals(chosenName))
+                                names.add(new BaseNavigationItem(func, chosenName, null));
+                        }
+                    }
+
+                }
+                return true;
+            }
+        });
+        return names.toArray(new NavigationItem[names.size()]);
     }
+
+    /**
+     * Wraps one entry to display in "Go To Symbol" dialog.
+     */
+    public static class BaseNavigationItem extends FakePsiElement {
+
+      private final PsiElement myPsiElement;
+      private final String myText;
+      private final Icon myIcon;
+
+      /**
+       * Creates a new display item.
+       *
+       * @param psiElement The PsiElement to navigate to.
+       * @param text       Text to show for this element.
+       * @param icon       Icon to show for this element.
+       */
+      public BaseNavigationItem(@NotNull PsiElement psiElement, @NotNull @NonNls String text, @Nullable Icon icon) {
+        myPsiElement = psiElement;
+        myText = text;
+        myIcon = icon;
+      }
+
+      public PsiElement getNavigationElement() {
+        return myPsiElement;
+      }
+
+      public Icon getIcon(boolean flags) {
+        return myIcon;
+      }
+
+      public ItemPresentation getPresentation() {
+        return new ItemPresentation() {
+
+          public String getPresentableText() {
+            return myText;
+          }
+
+          @Nullable
+          public String getLocationString() {
+            return '(' + myPsiElement.getContainingFile().getName() + ')';
+          }
+
+          @Nullable
+          public Icon getIcon(boolean open) {
+            return myIcon;
+          }
+
+          @Nullable
+          public TextAttributesKey getTextAttributesKey() {
+            return null;
+          }
+        };
+      }
+
+      public PsiElement getParent() {
+        return myPsiElement.getParent();
+      }
+
+      public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final BaseNavigationItem that = (BaseNavigationItem)o;
+
+        if (myPsiElement != null ? !myPsiElement.equals(that.myPsiElement) : that.myPsiElement != null) return false;
+        if (myText != null ? !myText.equals(that.myText) : that.myText != null) return false;
+
+        return true;
+      }
+
+      public int hashCode() {
+        int result;
+        result = (myPsiElement != null ? myPsiElement.hashCode() : 0);
+        result = 31 * result + (myText != null ? myText.hashCode() : 0);
+        return result;
+      }
+    }
+
+
 }
