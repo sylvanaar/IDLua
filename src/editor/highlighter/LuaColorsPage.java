79ca681cfaca620bd90ac841552c717742fbcb7c
sylvanaar <none@none>
2011-03-08 01:17:12 -0500

    remove color options that I dont want to be optional

--- src/editor/highlighter/LuaColorsPage.java
+++ src/editor/highlighter/LuaColorsPage.java
@@ -128,8 +128,8 @@ public class LuaColorsPage  implements ColorSettingsPage {
             new AttributesDescriptor(LuaBundle.message("color.settings.locals"), LuaHighlightingData.LOCAL_VAR),
             new AttributesDescriptor(LuaBundle.message("color.settings.field"), LuaHighlightingData.FIELD),
             new AttributesDescriptor(LuaBundle.message("color.settings.parameter"), LuaHighlightingData.PARAMETER),
-            new AttributesDescriptor(LuaBundle.message("color.settings.tailcall"), LuaHighlightingData.TAIL_CALL),
-            new AttributesDescriptor(LuaBundle.message("color.settings.upvalue"), LuaHighlightingData.UPVAL),
+//            new AttributesDescriptor(LuaBundle.message("color.settings.tailcall"), LuaHighlightingData.TAIL_CALL),
+//            new AttributesDescriptor(LuaBundle.message("color.settings.upvalue"), LuaHighlightingData.UPVAL),
           //  new AttributesDescriptor(LuaBundle.message("color.settings.identifier"), LuaHighlightingData.IDENTIFIER),
     };
 
