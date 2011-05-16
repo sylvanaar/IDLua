b9eb1774c6d2914d1579be4f1abc95e46bee9fef
sylvanaar <sylvanaar@mindspring.com>
2011-05-16 00:04:36 -0400

    change how we locate the stdlibrary files since we have relocated them

--- src/sdk/StdLibrary.java
+++ src/sdk/StdLibrary.java
@@ -16,6 +16,9 @@
 
 package com.sylvanaar.idea.Lua.sdk;
 
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.vfs.JarFileSystem;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -24,6 +27,8 @@ import com.intellij.openapi.vfs.newvfs.impl.VirtualDirectoryImpl;
 import com.intellij.util.PathUtil;
 import com.sylvanaar.idea.Lua.lang.psi.LuaPsiFile;
 
+import java.io.File;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Jon S Akhtar
@@ -31,15 +36,31 @@ import com.sylvanaar.idea.Lua.lang.psi.LuaPsiFile;
  * Time: 3:07 AM
  */
 public class StdLibrary {
+
+    public static final String STDLIBRARY = "stdlibrary";
+
     public static VirtualFile getStdFileLocation() {
+        IdeaPluginDescriptor descriptor = PluginManager.getPlugin(PluginId.getId("Lua"));
+        if (descriptor != null) {
+            File pluginPath = descriptor.getPath();
+
+            String url = VfsUtil.pathToUrl(pluginPath.getAbsolutePath());
+            VirtualFile dir = VirtualFileManager.getInstance().findFileByUrl(url);
+            if (dir != null)
+                dir = dir.findChild(STDLIBRARY);
+
+            if (dir != null)
+                return dir;
+        }
+
         String url = VfsUtil.pathToUrl(PathUtil.getJarPathForClass(LuaPsiFile.class));
         VirtualFile sdkFile = VirtualFileManager.getInstance().findFileByUrl(url);
         if (sdkFile != null) {
             VirtualFile jarFile = JarFileSystem.getInstance().getJarRootForLocalFile(sdkFile);
             if (jarFile != null) {
-                return jarFile.findChild("stdlibrary");
+                return jarFile.findChild(STDLIBRARY);
             } else if (sdkFile instanceof VirtualDirectoryImpl) {
-                return sdkFile.findChild("stdlibrary");
+                return sdkFile.findChild(STDLIBRARY);
             }
         }
 
