c47981f21cade9a52b5d82a889da839af0dabe60
sylvanaar <sylvanaar@mindspring.com>
2011-10-25 10:44:15 -0400

    IDLua-52: Add option to not show the tail call indicator
    --HG--
    branch : idea11

--- src/options/LuaApplicationSettings.java
+++ src/options/LuaApplicationSettings.java
@@ -39,6 +39,7 @@ import com.intellij.util.xmlb.XmlSerializerUtil;
 public class LuaApplicationSettings implements PersistentStateComponent<LuaApplicationSettings> {
     public boolean INCLUDE_ALL_FIELDS_IN_COMPLETIONS = false;
     public boolean RESOLVE_ALIASED_IDENTIFIERS = false;
+    public boolean SHOW_TAIL_CALLS_IN_GUTTER = true;
 
     @Override
     public LuaApplicationSettings getState() {
