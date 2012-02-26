b3b7b18e778ff9b1d98a605c8fa0dcb9480d06e8
sylvanaar <sylvanaar@mindspring.com>
2012-02-26 13:55:10 -0500

    type inference by default
    --HG--
    branch : idea11

--- src/options/LuaApplicationSettings.java
+++ src/options/LuaApplicationSettings.java
@@ -39,7 +39,7 @@ import com.intellij.util.xmlb.XmlSerializerUtil;
 public class LuaApplicationSettings implements PersistentStateComponent<LuaApplicationSettings> {
     public boolean INCLUDE_ALL_FIELDS_IN_COMPLETIONS = false;
     public boolean SHOW_TAIL_CALLS_IN_GUTTER = true;
-    public boolean ENABLE_TYPE_INFERENCE = false;
+    public boolean ENABLE_TYPE_INFERENCE = true;
 
     @Override
     public LuaApplicationSettings getState() {
