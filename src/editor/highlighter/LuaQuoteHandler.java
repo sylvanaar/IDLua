75071926e541f972e5ef7ca3467dbf6236ad322c
Peter Du <du@cs.columbia.edu>
2014-03-26 18:58:08 -0400

    Fix the LuaQuoteHandler
    --HG--
    branch : idea13

--- src/editor/highlighter/LuaQuoteHandler.java
+++ src/editor/highlighter/LuaQuoteHandler.java
@@ -1,21 +1,21 @@
-///*
-// * Copyright 2010 Jon S Akhtar (Sylvanaar)
-// *
-// *   Licensed under the Apache License, Version 2.0 (the "License");
-// *   you may not use this file except in compliance with the License.
-// *   You may obtain a copy of the License at
-// *
-// *   http://www.apache.org/licenses/LICENSE-2.0
-// *
-// *   Unless required by applicable law or agreed to in writing, software
-// *   distributed under the License is distributed on an "AS IS" BASIS,
-// *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// *   See the License for the specific language governing permissions and
-// *   limitations under the License.
-// */
-//
-//package com.sylvanaar.idea.Lua.editor.highlighter;
-//
+/*
+* Copyright 2010 Jon S Akhtar (Sylvanaar)
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+
+package com.sylvanaar.idea.Lua.editor.highlighter;
+
 //import com.intellij.codeInsight.editorActions.QuoteHandler;
 //import com.intellij.openapi.editor.Editor;
 //import com.intellij.openapi.editor.highlighter.HighlighterIterator;
@@ -28,6 +28,21 @@
 // * Date: Aug 1, 2010
 // * Time: 11:52:14 PM
 // */
+
+import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler;
+import com.sylvanaar.idea.Lua.lang.lexer.LuaTokenTypes;
+
+/**
+ * Fixing the Quote Handler - Peter
+ */
+public class LuaQuoteHandler extends SimpleTokenSetQuoteHandler {
+    public LuaQuoteHandler() {
+        super(
+                LuaTokenTypes.STRING_LITERAL_SET
+        );
+    }
+}
+
 //public class LuaQuoteHandler implements QuoteHandler {
 //    @Override
 //    public boolean isClosingQuote(HighlighterIterator highlighterIterator, int i) {
