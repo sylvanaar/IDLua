3793a92e8e12842e077d67be84cff51169a9a55d
tkl@vostrotouk <tkl@vostrotouk>
2014-05-12 16:41:31 +0200

    NPE fix for defining Lua SDK for linux
    --HG--
    branch : idea13
    extra : amend_source : bd3fd7c611c9cf9203971ee211e476971752023a

--- src/sdk/LuaSdkType.java
+++ src/sdk/LuaSdkType.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010 Jon S Akhtar (Sylvanaar)
- *  
+ *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
- *  
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *   Unless required by applicable law or agreed to in writing, software
  *   distributed under the License is distributed on an "AS IS" BASIS,
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -146,14 +146,17 @@ public class LuaSdkType extends SdkType {
         if (processOutput.getExitCode() != 0) {
             return null;
         }
-        final String stdout = processOutput.getStderr().trim();
-        if (stdout.isEmpty()) {
-            return null;
+        //Backwards compatibility - probably for Windows and OSX
+        final String stderr = processOutput.getStderr().trim();
+        if (!stderr.isEmpty()) {
+            return stderr.split(" ");
         }
-
-        String[] sa = stdout.split(" ");
-
-        return sa;
+        //linux
+        final String stdout = processOutput.getStdout().trim();
+        if (!stdout.isEmpty()) {
+            return stdout.split(" ");
+        }
+        return null;
     }
 
     @Nullable
